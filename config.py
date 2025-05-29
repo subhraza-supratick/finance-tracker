58e8a2bc402ab48468d5750b068010a3009593ef
subhraza-supratick <subhrazasupratick@gmail.com>
2025-05-29 21:59:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql://root:Riju@localhost/finance_tracker'
+        'mysql://uaeguuzfslfjcllx:ZBWIQgMLcrzGEsTw17Wq@bgqrmxzfrkb96jbracha-mysql.services.clever-cloud.com:3306/bgqrmxzfrkb96jbracha'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
